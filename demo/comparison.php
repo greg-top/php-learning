d9cd58047d0ee3e769cfde4fe4bc551ee8f39f00
gregory_sf9 <greg@studioforty9.com>
2018-05-01 09:24:29 +0100

    Chapter 3 - finished

---
+++ demo/comparison.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if (4 === "4") {
+
+    echo "it is true";
+
+}
+
+echo "<br>";
+
+if (4 == "4") {
+
+    echo "it is true 2";
+
+}
+
+echo "<br>";
+
+if (4 === "4" || 5 <10 ) {
+
+    echo "it is true 3";
+
+}
+
+echo "<br>";
+
+if (4 === "4" && 5 <10 ) {
+
+    echo "it is true 4";
+
+}
+
+?>
+
+</body>
+</html>
---
+++ demo/switch.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = 4;
+
+if ($number > 10) {
+    echo "it is";
+}
+
+if ($number > 15) {
+    echo "it is";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+//switch statement
+$number = 98;
+
+switch ($number) {
+    case 34:
+        echo "it is 34";
+        break;
+    case 44:
+        echo "it is 44";
+        break;
+    case 74:
+        echo "it is 74";
+        break;
+    default:
+        echo "default value";
+}
+
+
+
+
+
+?>
+
+</body>
+</html>
