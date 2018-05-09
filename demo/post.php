7507e12f41909514f9fce12be2d1c442b65832b0
gregory_sf9 <greg@studioforty9.com>
2018-05-09 17:15:46 +0100

    Used super global POST variable

---
+++ demo/post.php
@@ -0,0 +1,28 @@
+<?php   
+
+if ( isset($_POST['submit']) ) {
+    $name = $_POST['fname'];
+
+    echo $name;
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="post.php" method="post">
+    <input type="text" name="fname">
+    <input type="submit" name="submit">
+</form>
+
+</body>
+</html>
